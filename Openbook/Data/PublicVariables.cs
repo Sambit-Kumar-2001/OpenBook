553effcfc3a5ecdbe97060e1d4f639246065784e
Sambit Kumar Sahoo <sambit.sahoo@ajatus.co.in>
2024-02-13 23:56:25 +0530

    Add project file to main

---
+++ Openbook/Data/PublicVariables.cs
@@ -0,0 +1,8 @@
+﻿namespace Openbook.Data
+{
+    class PublicVariables
+    {
+        public static string _Symbol = "$";//Default Currency
+        public static string _navigation = string.Empty;
+    }
+}
